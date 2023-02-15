e81f3872c9607d458ee7e1ec05c721413b875e90
Eduardo Raiol <marquesraiol@gmail.com>
2023-02-14 21:33:45 -0300

    Aumentando as validações e criando dicas do chute

--- app/validacao.js
+++ app/validacao.js
@@ -2,11 +2,28 @@ function verificaSeOChutePossuiUmValorValido(chute) {
     const numero = +chute;
 
     if(chuteForInvalido(numero)) {
-        console.log('Valor inválido');
+        elementoChute.innerHTML += '<div>Valor inválido</div>';
+        return
     }
 
     if(numeroForManiorOUMenorQueOValorPermitido(numero)){
-        console.log(`Valor inválido: o número precisa estar entre ${menorValor} e ${maiorValor}`);
+        elementoChute.innerHTML +=`<div>Valor inválido: o número precisa estar entre ${menorValor} e ${maiorValor} </div>`;
+        return
+    }
+
+    if (numero === numeroSecreto){
+        document.body.innerHTML = `
+            <h2> Você acertou! </h2>
+            <h3> O número secreto era ${numeroSecreto}
+        `
+    }else if(numero > numeroSecreto){
+        elementoChute.innerHTML += `
+            <div>O número secreto é menor <i class="fa fa-arrow-down" aria-hidden="true"></i></div>
+        `
+    }else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa fa-arrow-up" aria-hidden="true"></i></div>
+    ` 
     }
 }
 
